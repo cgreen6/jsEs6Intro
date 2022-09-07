2d76834b885a436aa34c9c08e68277457e6b0581
Christian Green <hoolio.suare@gmail.com>
2022-09-06 18:54:53 -0600

    start of counter

---
+++ react_counter_fall22/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  
+  return (
+    <>
+      <h1>Counter example</h1>
+      <p></p>
+      <button>+</button>
+      <button>-</button>
+    </>
+  )
+}
+
+export default App;
---
+++ react_counter_fall22/src/components/counter/OLDApp.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class App extend Component {
+// class App extend React.Component {
+
+}
+
+export default App;
+
