b99fcd05387a81c846d28db29154e3cd6d343bc1
Christian Green <hoolio.suare@gmail.com>
2022-09-06 19:23:11 -0600

    created react project

--- react_counter_fall22/src/components/counter/OLDApp.js
+++ react_counter_fall22/src/components/counter/OLDApp.js
@@ -1,8 +1,38 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class App extend Component {
+class App extends Component {
 // class App extend React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { counterVal: 0 }
+    this.inc = this.inc.bind(this)
+    this.dec = this.dec.bind(this)
+  }
 
+  inc() {
+    this.setState({ 
+      counterVal: this.state.counterVal + 1 
+    })
+  }
+
+  dec() {
+    this.setState({ 
+      counterVal: this.state.counterVal - 1 
+    })
+  }
+
+  render() {
+    const { counterVal } = this.state
+    return (
+      <>
+        <h1>Counter example</h1>
+        {/* <p>{this.state.counterVal}</p> */}
+        <p>{counterVal}</p>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </>
+    )
+  }
 }
 
 export default App;
