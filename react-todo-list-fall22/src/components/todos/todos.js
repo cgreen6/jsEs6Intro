e20fe49add3ecb0d3c664ff4ed396a8a5ff0f219
Christian Green <hoolio.suare@gmail.com>
2022-09-06 20:47:13 -0600

    todos updated

--- react-todo-list-fall22/src/components/todos/todos.js
+++ react-todo-list-fall22/src/components/todos/todos.js
@@ -1,5 +1,6 @@
-
 import { useState } from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 const Todos = () => {
   const [todos, setTodos] = useState([
@@ -8,9 +9,25 @@ const Todos = () => {
     { id: 3, title: 'Graduate DPL', complete: false },
   ])
 
+  const getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  const addTodo = (todo) => {
+    // only for not having db 
+    const newTodo = { id: getUniqId, ...todo }
+    // const newTodo = { id: getUniqId, title: todo.title, complete: todo.complete }
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <>
-    
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
+      <TodoForm addTodo={addTodo} />
     </>
   )
 }
